be8a130809b88f37f5b879201f556f8a89faf1e1
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-01-21 11:11:03 -0800

    Passing handle change event as props

--- vite-type-project/src/App.tsx
+++ vite-type-project/src/App.tsx
@@ -1,43 +1,55 @@
 
 import './App.css'
+import { Button } from './components/Button'
 import { Greet } from './components/Greet'
 import { Heading } from './components/Heading'
+import { Input } from './components/Input'
 import { Osca } from './components/Osca'
 import { Person } from './components/Person'
 import { PersonList } from './components/PersonList'
 import { Status } from './components/Status'
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  }
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne'
-    },
-    {
-      first: 'Clack',
-      last: 'Kent'
-    },
-    {
-      first: 'Princess',
-      last: 'Diana'
-    }
-  ]
+  // const personName = {
+  //   first: 'Bruce',
+  //   last: 'Wayne',
+  // }
+  // const nameList = [
+  //   {
+  //     first: 'Bruce',
+  //     last: 'Wayne'
+  //   },
+  //   {
+  //     first: 'Clack',
+  //     last: 'Kent'
+  //   },
+  //   {
+  //     first: 'Princess',
+  //     last: 'Diana'
+  //   }
+  // ]
+
+  // const handleClick = (event) => {
+  //   let a = 5
+  //   let b = 5
+  //   console.log(a+b, event)
+  // }
    
 
   return (
     <div className="App">
-      <Greet name="Israel" isLoggedIn={true}/>
+      {/* <Greet name="Israel" isLoggedIn={true}/> */}
       {/* <Person name={personName}/> */}
-      <PersonList name = {nameList}/>
+      {/* <PersonList name = {nameList}/>
       <Status status='success'/>
       
       <Osca>
         <Heading>Oscar goes to Leonardo Siscap</Heading>
-      </Osca>
+      </Osca> */}
+      <Button handleClick={(event, id) => {
+        console.log("Button Clicked", event, id)
+      }}/>
+      <Input value="" handleChange={event=>console.log(event)} />
     </div>
   )
 }
